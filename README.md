a575363a89c04a9becdb6fa7d06e5673da1f66a0
alexpurhalo <alexpurhalo@gmail.com>
2016-08-19 16:51:06 +0300

    token-saving-to-local-storage

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 2. $ npm run dev
 3. browser: localhost:8080, rendering of "React simple starter" text 
 4. run your server by localhost:3090 address
-5. create user and remember his email address and password 
-6. use user's data to sign in by localhost:8080/signin address
-7. as output you can get browser access control error, so solve this and back to repeat previous step
-8. as output in console you should get 204 and 200, second with token in preview 
+5. sign in into existing account by localhost:8080/signin address 
+6. ensure that after authorization you has been redirected to localhost:8000/fetaure page
+7. check out token of authenticated user typing in console: localStorage.setItem('token') 
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,26 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
-	return function(dispatch) {
-		axios.post(`${ROOT_URL}/signin`, { email, password }); // email: email, password: password
-	};
 	// Submit email/password to the server
+	return function(dispatch) {
+		axios.post(`${ROOT_URL}/signin`, { email, password }).then(response => {
+			// If request is good.
+			// - Update state to indicate user is authenticated
+			dispatch({ type: AUTH_USER });
 
-	// If request is good.
-	// - Update state to indicate user is authenticated
-	// - Save the JWT token
-	// - redirect to the route '/feature'
+			// - Save the JWT token
+			localStorage.setItem('token', response.data.token);
+			// in browser console try as input: localStorage.setItem('token'), output: token info
 
-	// If request is bad.
-	// - Show an error to the user
+			// - Redirect to the route '/feature'
+			browserHistory.push('/feature')
+		}).catch(() => {
+			// If request is bad.
+			// - Show an error to the user
+		})
+	};
 }
