90a64cfef6810e5cc82b20fd6e219ce95e21a12c
alexpurhalo <alexpurhalo@gmail.com>
2016-08-19 17:34:25 +0300

    displaying-error-message

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 2. $ npm run dev
 3. browser: localhost:8080, rendering of "React simple starter" text 
 4. run your server by localhost:3090 address
-5. sign in into existing account by localhost:8080/signin address 
-6. ensure that after authorization you has been redirected to localhost:8000/fetaure page
-7. check out token of authenticated user typing in console: localStorage.setItem('token') 
+5. sign in with wrong data by localhost:8080/signin
+6. displaying error message 
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -9,6 +9,16 @@ class Signin extends Component {
 		this.props.signinUser({ email, password });
 	}
 
+	renderAlert() {
+		if (this.props.errorMessage) {
+			return (
+				<div className="alert alert-danger">
+					<strong>Oops!</strong> {this.props.errorMessage}
+				</div>
+			);
+		}
+	}
+
 	render() {
 		const { handleSubmit, fields: { email, password }} = this.props;
 		return (
@@ -21,13 +31,17 @@ class Signin extends Component {
 					<label>Password:</label>
 					<input {...password}className="form-control"/>
 				</fieldset>
+				{this.renderAlert()}
 				<button action="submit" className="btn btn-primary">Sign in</button>
 			</form>
 		);
 	}
 }
 
+function mapStateToProps(state) {
+	return { errorMessage: state.auth.error };
+}
 export default reduxForm({
 	form: 'signin',
 	fields: ['email', 'password']
-}, null, actions)(Signin); // instead of null here could be mapStateToProps
+}, mapStateToProps, actions)(Signin); // instead of null here could be mapStateToProps
