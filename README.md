f542ff714c923f7070cff5978a3a9ae62ad9a6e7
alexpurhalo <alexpurhalo@gmail.com>
2016-08-19 22:36:12 +0300

    sign-up-form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 1. $ sudo npm install
 2. $ npm run dev
 3. run your server by localhost:3090 address
-4 . in browser visit localhost:8000/signup and ensure that you have "Sign Up page" text
+4 . in browser visit localhost:8000/signup and ensure that we have form with email and passwords fields
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
-export default class Signup extends Component {
+class Signup extends Component {
 	render() {
+		const { handlerSubmit, fields: { email, password, passwordConfirm}} = this.props;
+
 		return (
-			<div>
-				<h1>Sign Up page</h1>
-			</div>
+			<form>
+				<fieldset className="form-group">
+					<label>Email:</label>
+					<input className="form-control" {...email} type="email" />
+				</fieldset>
+				<fieldset className="form-group">
+					<label>Password:</label>
+					<input className="form-control" {...password} type="password" />
+				</fieldset>
+				<fieldset className="form-group">
+					<label>Confirm Password:</label>
+					<input className="form-control" {...passwordConfirm} type="password" />
+				</fieldset>
+				<button action="submit" className="btn btn-primary">Sign Up</button>
+			</form>
 		);
 	}
 }
 
+export default reduxForm({
+	form: 'signup',
+	fields: ['email', 'password', 'passwordConfirm']
+}, null, actions)(Signup);
