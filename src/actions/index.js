e0f5a54b4c5cbdaef44bcf3425fbd359b2ade7e9
alexpurhalo <alexpurhalo@gmail.com>
2016-08-22 22:33:49 +0300

    aaa

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from './types';
 
-const ROOT_URL = 'http://localhost:3090';
+const ROOT_URL = 'https://shielded-sierra-72646.herokuapp.com';
 
 export function signinUser({ email, password }) {
 	return function(dispatch) {
 		// Submit email/password to the server
-		axios.post(`${ROOT_URL}/signin`, { email, password })
+		axios.post(`${ROOT_URL}/sign_in`, { email, password })
 			.then(response => {
 				// If request is good...
 				// - Update state to indicate user is authenticated
 				dispatch({ type: AUTH_USER });
 				// - Save the JWT token
-				localStorage.setItem('token', response.data.token);
+				localStorage.setItem('token', response.data.access_token);
 				// - redirect to the route '/feature'
 				browserHistory.push('/feature');
 			})
