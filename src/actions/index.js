7fde3eea853c8fcc01cd79111b82e6d471d0ead0
alexpurhalo <alexpurhalo@gmail.com>
2016-08-23 10:14:14 +0300

    sign-up-address

--- src/actions/index.js
+++ src/actions/index.js
@@ -27,10 +27,10 @@ export function signinUser({ email, password }) {
 
 export function signupUser({ email, password }) {
 	return function(dispatch) {
-		axios.post(`${ROOT_URL}/signup`, { email, password })
+		axios.post(`${ROOT_URL}/users`, { user: { email, password } })
 			.then(response => {
 				dispatch({ type: AUTH_USER });
-				localStorage.setItem('token', response.data.token);
+				localStorage.setItem('token', response.data.access_token);
 				browserHistory.push('/feature');
 			})
 			.catch(response => dispatch(authError(response.data.error)));
